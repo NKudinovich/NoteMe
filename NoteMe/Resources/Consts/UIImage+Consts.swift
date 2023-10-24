79205e5768d6dd4cc1e5506c488bc2ed3c8c0728
Nikita Kudinovich <kudinovich_2011@mail.ru>
2023-10-24 20:52:15 +0300

    -Added SnapKit -Added Constants

---
+++ NoteMe/Resources/Consts/UIImage+Consts.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+Consts.swift
+//  NoteMe
+//
+//  Created by Nikita Kudinovich on 24.10.23.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    //MARK: - General
+    enum General {
+        
+        static let logo: UIImage = .init(named: "logo")!
+        
+    }
+}
